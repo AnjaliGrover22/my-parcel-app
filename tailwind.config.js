70c58a9080bf7f4be8d2b2c39c01925142f847d8
AnjaliGrover22 <anjaligrover2210@gmail.com>
2024-07-16 21:34:13 +0200

    set attributes and added map coordinates

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["index.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+// module.exports = {
+//   add: function(a, b) {
+//     return a + b;
+//   }
+// };
